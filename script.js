488c83a6f88861b4dfaa9393c2c068f1a900beb5
Emmabee-initiatives <138867283+Emmabee-initiatives@users.noreply.github.com>
2023-09-16 10:33:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let  Navlinks = document.getElementById("NavLinks");
+      
+        
+        
+        function showmenu(){
+            Nav-links.style.right = "-200px";
+            }
+        
+        function hidemenu(){
+            Nav-links.style.right = "0";
+        }
+        
