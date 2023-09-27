4d8929683577fffeda7a322306374c6921a30cba
Emmabee-initiatives <138867283+Emmabee-initiatives@users.noreply.github.com>
2023-09-27 11:55:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let  Navlinks = document.getElementById("NavLinks");
         
         
         function showmenu(){
-            Nav-links.style.right = "-200px";
+            Navlinks.style.right = "-200px";
             }
         
         function hidemenu(){
-            Nav-links.style.right = "0";
+            Navlinks.style.right = "0";
         }
         
